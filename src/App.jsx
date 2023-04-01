4c3ed1fdabfb6fa479dfb71c6657a31f963e5254
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-01 21:14:42 +0300

    Add initial layout styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
-    return <h1>Hello!</h1>;
+    return (
+      <div className="app">
+        <section className="input-section">inputs</section>
+        <section className="preview-section">preview</section>
+      </div>
+    );
   }
 }
 

88ebc8cf7fc0b4ed6360bf154133e6d0728730f9
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-16 13:58:55 +0300

    Change GeneralForm into a functional component

--- src/components/GeneralForm.jsx
+++ src/components/GeneralForm.jsx
@@ -1,46 +1,31 @@
-import React, { Component } from 'react';
-
-class GeneralForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    const { updateGeneral } = this.props;
+export default function GeneralForm({ updateGeneral, general }) {
+  function handleChange(e) {
     updateGeneral(e.target);
   }
 
-  render() {
-    const { general } = this.props;
-
-    return (
-      <div className="form-container">
-        <h2 className="form-title">General Info</h2>
-        <div>
-          <form className="form">
-            <div className="form-section">
-              <label htmlFor="name">Name</label>
-              <input type="text" id="name" name="name" value={general.name} onChange={this.handleChange} />
-            </div>
-            <div className="form-section">
-              <label htmlFor="title">Title</label>
-              <input type="text" id="title" name="title" value={general.title} onChange={this.handleChange} />
-            </div>
-            <div className="form-section">
-              <label htmlFor="email">e-mail</label>
-              <input type="email" id="email" name="email" value={general.email} onChange={this.handleChange} />
-            </div>
-            <div className="form-section">
-              <label htmlFor="phone">Phone number</label>
-              <input type="tel" id="phone" name="phone" value={general.phone} onChange={this.handleChange} />
-            </div>
-          </form>
-        </div>
+  return (
+    <div className="form-container">
+      <h2 className="form-title">General Info</h2>
+      <div>
+        <form className="form">
+          <div className="form-section">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" value={general.name} onChange={handleChange} />
+          </div>
+          <div className="form-section">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" value={general.title} onChange={handleChange} />
+          </div>
+          <div className="form-section">
+            <label htmlFor="email">e-mail</label>
+            <input type="email" id="email" name="email" value={general.email} onChange={handleChange} />
+          </div>
+          <div className="form-section">
+            <label htmlFor="phone">Phone number</label>
+            <input type="tel" id="phone" name="phone" value={general.phone} onChange={handleChange} />
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default GeneralForm;

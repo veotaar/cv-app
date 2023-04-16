d68ff66ab2e94e08a3d9657d566089f7ce3c2d2d
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-16 13:47:03 +0300

    Change EducationPreview into a functional component

--- src/components/EducationPreview.jsx
+++ src/components/EducationPreview.jsx
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
 import '../styles/EducationPreview.css';
 
-class EducationPreview extends Component {
-  render() {
-    const { education } = this.props;
-    return (
-      <div className="education-cv">
-        <h2 className="education-title">Education</h2>
-        {/* <hr /> */}
-        {education.map(({ key, schoolName, degreeType, field, startDate, endDate }) => (
-          <div className="education-container" key={key}>
-            <div className="education-details">
-              <p className="education-name">{schoolName}</p>
-              <p className="education-dates">
-                {startDate} - {endDate}
-              </p>
-            </div>
-
-            <p className="education-field">
-              {degreeType}, {field}
+export default function EducationPreview({ education }) {
+  return (
+    <div className="education-cv">
+      <h2 className="education-title">Education</h2>
+      {/* <hr /> */}
+      {education.map(({ key, schoolName, degreeType, field, startDate, endDate }) => (
+        <div className="education-container" key={key}>
+          <div className="education-details">
+            <p className="education-name">{schoolName}</p>
+            <p className="education-dates">
+              {startDate} - {endDate}
             </p>
           </div>
-        ))}
-      </div>
-    );
-  }
-}
 
-export default EducationPreview;
+          <p className="education-field">
+            {degreeType}, {field}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}

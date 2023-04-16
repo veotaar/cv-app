eda2121e2d5bf2c89cee8a6461d4e49806ccbab9
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-16 14:02:18 +0300

    Change SummaryForm into a functional component

--- src/components/SummaryForm.jsx
+++ src/components/SummaryForm.jsx
@@ -1,33 +1,19 @@
-import React, { Component } from 'react';
-
-class SummaryForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    const { updateSummary } = this.props;
+export default function SummaryForm({ updateSummary, summary }) {
+  function handleChange(e) {
     updateSummary(e.target);
   }
 
-  render() {
-    const { summary } = this.props;
-    return (
-      <div className="form-container">
-        <h2 className="form-title">Summary</h2>
-        <div>
-          <form className="form">
-            <div className="form-section">
-              <label htmlFor="summary">Summary Text</label>
-              <textarea name="summary" id="summary" value={summary} onChange={this.handleChange} />
-            </div>
-          </form>
-        </div>
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Summary</h2>
+      <div>
+        <form className="form">
+          <div className="form-section">
+            <label htmlFor="summary">Summary Text</label>
+            <textarea name="summary" id="summary" value={summary} onChange={handleChange} />
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default SummaryForm;

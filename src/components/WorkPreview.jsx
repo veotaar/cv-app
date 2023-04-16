2e673066edc9eefa1926949df850d8005646c0a2
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-16 13:41:38 +0300

    Change WorkPreview into a functional component

--- src/components/WorkPreview.jsx
+++ src/components/WorkPreview.jsx
@@ -1,29 +1,23 @@
-import React, { Component } from 'react';
 import '../styles/WorkPreview.css';
 
-class WorkPreview extends Component {
-  render() {
-    const { work } = this.props;
-    return (
-      <div className="work-cv">
-        <h2 className="work-title">Work Experience</h2>
-        {/* <hr /> */}
-        {work.map(({ key, companyName, position, tasks, startDate, endDate }) => (
-          <div className="company-container" key={key}>
-            <div className="company-details">
-              <p className="company-name">{companyName}</p>
-              <p className="company-dates">
-                {startDate} - {endDate}
-              </p>
-            </div>
-
-            <p className="company-position">{position}</p>
-            <p className="company-tasks">{tasks}</p>
+export default function WorkPreview({ work }) {
+  return (
+    <div className="work-cv">
+      <h2 className="work-title">Work Experience</h2>
+      {/* <hr /> */}
+      {work.map(({ key, companyName, position, tasks, startDate, endDate }) => (
+        <div className="company-container" key={key}>
+          <div className="company-details">
+            <p className="company-name">{companyName}</p>
+            <p className="company-dates">
+              {startDate} - {endDate}
+            </p>
           </div>
-        ))}
-      </div>
-    );
-  }
-}
 
-export default WorkPreview;
+          <p className="company-position">{position}</p>
+          <p className="company-tasks">{tasks}</p>
+        </div>
+      ))}
+    </div>
+  );
+}

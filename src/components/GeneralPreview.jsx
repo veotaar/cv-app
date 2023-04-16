9be351cb529594fc713d2ba2591343884382fd2c
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-16 13:45:47 +0300

    Change GeneralPreview into a functional component

--- src/components/GeneralPreview.jsx
+++ src/components/GeneralPreview.jsx
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
 import '../styles/GeneralPreview.css';
 
-class GeneralPreview extends Component {
-  render() {
-    const { general } = this.props;
-    return (
-      <div>
-        <div className="general-cv">
-          <div className="general-heading">
-            <h1 className="general-name">{general.name}</h1>
-            <h2 className="general-title">{general.title}</h2>
-          </div>
-          <div className="general-contact">
-            <p>{general.email}</p>
-            <p>{general.phone}</p>
-          </div>
+export default function GeneralPreview({ general }) {
+  return (
+    <div>
+      <div className="general-cv">
+        <div className="general-heading">
+          <h1 className="general-name">{general.name}</h1>
+          <h2 className="general-title">{general.title}</h2>
         </div>
-        <hr className="general-hr" />
+        <div className="general-contact">
+          <p>{general.email}</p>
+          <p>{general.phone}</p>
+        </div>
       </div>
-    );
-  }
+      <hr className="general-hr" />
+    </div>
+  );
 }
-
-export default GeneralPreview;

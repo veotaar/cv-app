7000d8ae0f83302f9eb02696a1687528df0a706c
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-16 13:43:45 +0300

    Change SummaryPreview into a functional component

--- src/components/SummaryPreview.jsx
+++ src/components/SummaryPreview.jsx
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
 import '../styles/SummaryPreview.css';
 
-class SummaryPreview extends Component {
-  render() {
-    const { summary } = this.props;
-    return (
-      <div className="summary-cv">
-        <h2 className="summary-title">Summary</h2>
-        {/* <hr /> */}
-        <div className="summary-container">
-          <p>{summary}</p>
-        </div>
+export default function SummaryPreview({ summary }) {
+  return (
+    <div className="summary-cv">
+      <h2 className="summary-title">Summary</h2>
+      {/* <hr /> */}
+      <div className="summary-container">
+        <p>{summary}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default SummaryPreview;

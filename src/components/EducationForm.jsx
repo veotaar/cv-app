e803c7c43836dde4074d32fdc8bb825bdc634cfb
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-16 13:56:06 +0300

    Change EducationForm into a functional component

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,101 +1,76 @@
-import React, { Component } from 'react';
-
-class EducationForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleAdd = this.handleAdd.bind(this);
-  }
-
-  handleChange(e) {
-    const { updateEducation } = this.props;
+export default function EducationForm({ education, updateEducation, addEducationEntry, deleteEducationEntry }) {
+  function handleChange(e) {
     updateEducation(e.target);
   }
 
-  handleDelete(e) {
-    const { deleteEducationEntry } = this.props;
+  function handleDelete(e) {
     deleteEducationEntry(e.target.id);
   }
 
-  handleAdd() {
-    const { addEducationEntry } = this.props;
+  function handleAdd() {
     addEducationEntry();
   }
 
-  render() {
-    const { education } = this.props; // array of objects
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Education</h2>
 
-    return (
-      <div className="form-container">
-        <h2 className="form-title">Education</h2>
-
-        <div>
-          {education.map((item) => (
-            <form className="form" key={item.key}>
-              <div className="form-section">
-                <label htmlFor={`schoolName-${item.key}`}>School</label>
-                <input
-                  type="text"
-                  id={`schoolName-${item.key}`}
-                  name="schoolName"
-                  value={item.schoolName}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="form-section">
-                <label htmlFor={`degreeType-${item.key}`}>Degree</label>
-                <input
-                  type="text"
-                  id={`degreeType-${item.key}`}
-                  name="degreeType"
-                  value={item.degreeType}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="form-section">
-                <label htmlFor={`field-${item.key}`}>Field</label>
-                <input
-                  type="text"
-                  id={`field-${item.key}`}
-                  name="field"
-                  value={item.field}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="form-section">
-                <label htmlFor={`startDate-${item.key}`}>Start</label>
-                <input
-                  type="text"
-                  id={`startDate-${item.key}`}
-                  name="startDate"
-                  value={item.startDate}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="form-section">
-                <label htmlFor={`endDate-${item.key}`}>End</label>
-                <input
-                  type="text"
-                  id={`endDate-${item.key}`}
-                  name="endDate"
-                  value={item.endDate}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <button className="button button-delete" type="button" onClick={this.handleDelete} id={item.key}>
-                Delete
-              </button>
-            </form>
-          ))}
-        </div>
-        <button className="button button-add" type="button" onClick={this.handleAdd}>
-          + Add More
-        </button>
+      <div>
+        {education.map((item) => (
+          <form className="form" key={item.key}>
+            <div className="form-section">
+              <label htmlFor={`schoolName-${item.key}`}>School</label>
+              <input
+                type="text"
+                id={`schoolName-${item.key}`}
+                name="schoolName"
+                value={item.schoolName}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-section">
+              <label htmlFor={`degreeType-${item.key}`}>Degree</label>
+              <input
+                type="text"
+                id={`degreeType-${item.key}`}
+                name="degreeType"
+                value={item.degreeType}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-section">
+              <label htmlFor={`field-${item.key}`}>Field</label>
+              <input type="text" id={`field-${item.key}`} name="field" value={item.field} onChange={handleChange} />
+            </div>
+            <div className="form-section">
+              <label htmlFor={`startDate-${item.key}`}>Start</label>
+              <input
+                type="text"
+                id={`startDate-${item.key}`}
+                name="startDate"
+                value={item.startDate}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-section">
+              <label htmlFor={`endDate-${item.key}`}>End</label>
+              <input
+                type="text"
+                id={`endDate-${item.key}`}
+                name="endDate"
+                value={item.endDate}
+                onChange={handleChange}
+              />
+            </div>
+            <button className="button button-delete" type="button" onClick={handleDelete} id={item.key}>
+              Delete
+            </button>
+          </form>
+        ))}
       </div>
-    );
-  }
+      <button className="button button-add" type="button" onClick={handleAdd}>
+        + Add More
+      </button>
+    </div>
+  );
 }
-
-export default EducationForm;
